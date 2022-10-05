7e8d09d9cc2d0a16824ad8d5f6b3158420f4a0d6
Jean-baptiste DUBILLARD <jdubilla@e2r9p4.clusters.42paris.fr>
2022-10-05 16:04:28 +0200

    Avancement parsing de la map + norme + segfault

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jdubilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 13:49:28 by jdubilla          #+#    #+#             */
-/*   Updated: 2022/10/05 15:37:14 by jdubilla         ###   ########.fr       */
+/*   Updated: 2022/10/05 16:03:14 by jdubilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(int argc, char **argv)
 	init_struct(&root);
 	if (check_error(argc, argv, &root))
 		free_struct_exit(&root);
+	for (int i = 0; root.map[i]; i++)
+		printf("%s\n", root.map[i]);
 	// start_cub3d(&root);
 	free_struct_exit(&root);
 	return (0);
