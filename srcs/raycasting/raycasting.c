6adb941b98227bd7f7e43b880464b40bfa00fc74
pareidolies <sacha.mkara@gmail.com>
2022-10-13 17:44:11 +0200

    orientation of textures fixed

--- srcs/raycasting/raycasting.c
+++ srcs/raycasting/raycasting.c
@@ -56,14 +56,14 @@ void	compute_wallx(t_ray *ray)
 	if (ray->side == HORIZONTAL) //HORI
 	{
 		ray->xpm->id = 0;
-		if (ray->dir.x > 0)
+		if (ray->raydir.x > 0)
 			ray->xpm->id = 1;
 		ray->wallx = ray->pos.y + ray->perpwalldist * ray->raydir.y;
 	}
 	else
 	{
 		ray->xpm->id = 2;
-		if (ray->dir.y < 0)
+		if (ray->raydir.y < 0)
 			ray->xpm->id = 3;
 		ray->wallx = ray->pos.x + ray->perpwalldist * ray->raydir.x;
 	}
