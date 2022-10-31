64b90530454052b3bf8cdfab0b6c873549c88705
pareidolies <sacha.mkara@gmail.com>
2022-10-31 14:22:31 +0100

    working on sprites...

--- srcs/raycasting/sprites.c
+++ srcs/raycasting/sprites.c
@@ -78,22 +78,43 @@ void    get_sprites_coordinates(t_ray *ray, t_sprite *sprite)
 
 void    sort_sprites(t_ray *ray, t_sprite *sprite)
 {
-    int k;
+    int i;
+    int j;
+    double  tmp;
 
-    k = 0;
-    while (k < sprite->nbr)
+    i = 0;
+    while (i < sprite->nbr)
     {
-        sprite->order[k] = k;
-        sprite->distance[k] = ((ray->pos.x - sprite->tab[k].x) * (ray->pos.x - sprite->tab[k].x) + (ray->pos.y - sprite->tab[k].y) * (ray->pos.y - sprite->tab[k].y)); //sqrt not taken, unneeded
-        k++;
+        sprite->order[i] = i;
+        sprite->distance[i] = ((ray->pos.x - sprite->tab[i].x) * (ray->pos.x - sprite->tab[i].x) + (ray->pos.y - sprite->tab[i].y) * (ray->pos.y - sprite->tab[i].y)); //sqrt not taken, unneeded
+        i++;
+    }
+    i = 0;
+    while (i < sprite->nbr)
+    {
+        j = 0;
+		while (j < sprite->nbr - 1)
+		{
+			if (sprite->distance[j] < sprite->distance[j + 1])
+			{
+				tmp = sprite->distance[j];
+				sprite->distance[j] = sprite->distance[j + 1];
+				sprite->distance[j + 1] = tmp;
+				tmp = sprite->order[j];
+				sprite->order[j] = sprite->order[j + 1];
+				sprite->order[j + 1] = (int)tmp;
+			}
+            j++;
+		}
+        i++;
     }
 }
 
-void    draw_sprites()
+/*void    draw_sprites(t_ray *ray, t_sprite *sprite)
 {
 
 
-}
+}*/
 
 void    add_sprites(t_ray *ray)
 {
@@ -102,7 +123,10 @@ void    add_sprites(t_ray *ray)
     get_sprites_nbr(ray, &sprite);
     get_sprites_coordinates(ray, &sprite);
     //CHECK
-    //printf("sprite number : %d\n", sprite.nbr);
-    //printf("x : %f\n", sprite.tab[0].x);
-    //printf("y : %f\n", sprite.tab[0].y);
+    printf("sprite number : %d\n", sprite.nbr);
+    printf("x : %f\n", sprite.tab[0].x);
+    printf("y : %f\n", sprite.tab[0].y);
+    printf("x : %f\n", sprite.tab[1].x);
+    printf("y : %f\n", sprite.tab[1].y);
+    sort_sprites(ray, &sprite);
 }
