44f9fcee83811781525a19e1ae2c60ef3e903454
Jean-baptiste DUBILLARD <jdubilla@e1r11p6.clusters.42paris.fr>
2022-11-03 21:16:58 +0100

    Norme + color

--- srcs_bonus/main.c
+++ srcs_bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: jdubilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 13:49:28 by jdubilla          #+#    #+#             */
-/*   Updated: 2022/11/02 09:23:19 by jdubilla         ###   ########.fr       */
+/*   Updated: 2022/11/03 21:16:28 by jdubilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,3 @@ int	main(int argc, char **argv)
 	start_raycasting(&root);
 	return (0);
 }
-
-/*
-
-	- Segfault si les textures n'existent pas
-
-	- Recuperer le PATH des portes depuis le fichier 
-	et non directement dans le code comme actuellement
-
-*/
